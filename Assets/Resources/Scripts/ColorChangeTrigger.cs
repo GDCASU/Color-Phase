bd06433dbc868375fd06c8137ce6508fda2f2c19
Kevin12525 <40506166+Kevin12525@users.noreply.github.com>
2018-07-10 18:23:48 -0700

    Updated ColorChange

--- Assets/Resources/Scripts/ColorChangeTrigger.cs
+++ Assets/Resources/Scripts/ColorChangeTrigger.cs
@@ -15,48 +15,12 @@ public class ColorChangeTrigger : MonoBehaviour {
     public void OnTriggerEnter(Collider other)
     {
         //Debug.Log(other);
-        if (other.gameObject.tag == "Player 1")
+        if (other.tag.StartsWith("Player"))
         {
             if (playerColor1.currentColor != colorValue)
             {
-                playerColor1.setColor(colorValue);
-                playerColor1.currentColor = colorValue;
-                if (destroyOnContact)
-                {
-                    Destroy(this.gameObject);
-                }
-            }
-        }
-        if (other.gameObject.tag == "Player 2")
-        {
-            if (playerColor2.currentColor != colorValue)
-            {
-                playerColor2.setColor(colorValue);
-                playerColor2.currentColor = colorValue;
-                if (destroyOnContact)
-                {
-                    Destroy(this.gameObject);
-                }
-            }
-        }
-        if (other.gameObject.tag == "Player 3")
-        {
-            if (playerColor3.currentColor != colorValue)
-            {
-                playerColor3.setColor(colorValue);
-                playerColor3.currentColor = colorValue;
-                if (destroyOnContact)
-                {
-                    Destroy(this.gameObject);
-                }
-            }
-        }
-        if (other.gameObject.tag == "Player 4")
-        {
-            if (playerColor4.currentColor != colorValue)
-            {
-                playerColor4.setColor(colorValue);
-                playerColor4.currentColor = colorValue;
+                other.GetComponent<ColorSwap>().setColor(colorValue);
+                other.GetComponent<ColorSwap>().currentColor = colorValue;
                 if (destroyOnContact)
                 {
                     Destroy(this.gameObject);
