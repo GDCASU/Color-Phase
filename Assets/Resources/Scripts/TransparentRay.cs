103c93a82861065564f8cd78b69dd78f3786ba36
Kevin12525 <40506166+Kevin12525@users.noreply.github.com>
2018-07-10 23:37:32 -0700

    Added object alpha fade

---
+++ Assets/Resources/Scripts/TransparentRay.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparentRay : MonoBehaviour {
+
+
+    private Stack<transparentObjectClass> transparentObjects=new Stack<transparentObjectClass>();
+    public Material tranparent;
+    public int resetCounter=0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// This code only works if the camera is centered on the player
+	void Update () {
+        RaycastHit hit;
+        
+        if (resetCounter == 0)
+        {
+            
+            try
+            //while (transparentObjects.Peek() != null)
+            {
+                transparentObjectClass reset=transparentObjects.Pop();
+                reset.transparentObject.GetComponent<Renderer>().material = reset.originalMaterial;
+                reset.transparentObject.layer = reset.originalLayer;
+            }
+            catch
+            {
+
+            }
+        }
+        
+        if (Physics.Raycast(transform.position,transform.forward,out hit))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            if (!hit.collider.tag.StartsWith("Player"))
+            {
+                GameObject G = hit.collider.gameObject;
+                transparentObjects.Push(new transparentObjectClass(G, G.GetComponent<Renderer>().material,G.layer));
+                G.GetComponent<Renderer>().material = tranparent;
+                G.layer = 2;
+            }
+        }
+        resetCounter= (resetCounter+1)%2000;
+
+    }
+    private class transparentObjectClass
+    {
+        public GameObject transparentObject;
+        public Material originalMaterial;
+        public int originalLayer;
+        public transparentObjectClass(GameObject to, Material om,int ol)
+        {
+            transparentObject = to;
+            originalMaterial = om;
+            originalLayer = ol;
+        }
+    }
+}
