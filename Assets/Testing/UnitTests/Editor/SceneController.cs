b3c67d40780ad50f3aff03e927011c92abdea053
Trevor Angle <trevoraangle@gmail.com>
2018-07-11 22:02:35 -0700

    Set up unit testing file

---
+++ Assets/Testing/UnitTests/Editor/SceneController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+namespace Testing.UnitTests.Editor
+{
+	public class SceneController {
+
+		[Test]
+		public void SceneControllerSimplePasses() {
+			// Use the Assert class to test conditions.
+			Assert.IsTrue(true);
+		}
+
+		// A UnityTest behaves like a coroutine in PlayMode
+		// and allows you to yield null to skip a frame in EditMode
+		[UnityTest]
+		public IEnumerator SceneControllerWithEnumeratorPasses() {
+			// Use the Assert class to test conditions.
+			// yield to skip a frame
+			yield return null;
+		}
+	}
+}
+
