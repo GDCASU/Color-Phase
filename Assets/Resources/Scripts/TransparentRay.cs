cce998abcf46a15b2f8cf963af728673eb6bd41d
Kevin12525 <40506166+Kevin12525@users.noreply.github.com>
2018-07-12 20:41:02 -0700

    Transparent walls are now only seen by the player looking through
    I put a duplicate wall at the same positionl.  The ray shifts the wall to a different level, not seen by the centered player but seen by all others.  Each player would need their own layer.

--- Assets/Resources/Scripts/TransparentRay.cs
+++ Assets/Resources/Scripts/TransparentRay.cs
@@ -8,12 +8,15 @@ public class TransparentRay : MonoBehaviour {
     private Stack<transparentObjectClass> transparentObjects=new Stack<transparentObjectClass>();
     public Material tranparent;
     public int resetCounter=0;
+
+    private int layerMask=1<<2;
     // Use this for initialization
     void Start () {
-		
+        layerMask = ~(layerMask | (1 << 17));
 	}
 	
 	// This code only works if the camera is centered on the player
+    //Else add a seperate tag for the ground
 	void Update () {
         RaycastHit hit;
         
@@ -33,15 +36,22 @@ public class TransparentRay : MonoBehaviour {
             }
         }
         
-        if (Physics.Raycast(transform.position,transform.forward,out hit))
+        if (Physics.Raycast(transform.position,transform.forward,out hit,100,layerMask))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
             if (!hit.collider.tag.StartsWith("Player"))
             {
                 GameObject G = hit.collider.gameObject;
                 transparentObjects.Push(new transparentObjectClass(G, G.GetComponent<Renderer>().material,G.layer));
-                G.GetComponent<Renderer>().material = tranparent;
-                G.layer = 2;
+                if (!hit.collider.tag.StartsWith("PermamentWall"))
+                {
+                    G.GetComponent<Renderer>().material = tranparent;
+                    G.layer = 2;
+                }
+                else
+                {
+                    G.layer = 17;
+                }
             }
         }
         resetCounter= (resetCounter+1)%2000;
