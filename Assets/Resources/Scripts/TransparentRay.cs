3b5f24638927555b44bdab098f50a9609b8e6afb
Kevin12525 <40506166+Kevin12525@users.noreply.github.com>
2018-07-17 14:56:59 -0700

    Fixed camera pulsing

--- Assets/Resources/Scripts/TransparentRay.cs
+++ Assets/Resources/Scripts/TransparentRay.cs
@@ -8,6 +8,7 @@ public class TransparentRay : MonoBehaviour {
     private Stack<transparentObjectClass> transparentObjects=new Stack<transparentObjectClass>();
     public Material tranparent;
     public int resetCounter=0;
+    public GameObject[] players;
 
     private int layerMask=1<<2;
     // Use this for initialization
@@ -26,35 +27,46 @@ public class TransparentRay : MonoBehaviour {
             try
             //while (transparentObjects.Peek() != null)
             {
-                transparentObjectClass reset=transparentObjects.Pop();
-                reset.transparentObject.GetComponent<Renderer>().material = reset.originalMaterial;
-                reset.transparentObject.layer = reset.originalLayer;
+                while (transparentObjects.Peek() != null)
+                {
+                    transparentObjectClass reset = transparentObjects.Pop();
+                    reset.transparentObject.GetComponent<Renderer>().material = reset.originalMaterial;
+                    reset.transparentObject.layer = reset.originalLayer;
+                }
             }
             catch
             {
 
             }
         }
-        
-        if (Physics.Raycast(transform.position,transform.forward,out hit,100,layerMask))
+        for (int x = 0; x < players.Length; x++)
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            if (!hit.collider.tag.StartsWith("Player"))
+            bool keepGoing= true;
+            while (keepGoing)
             {
-                GameObject G = hit.collider.gameObject;
-                transparentObjects.Push(new transparentObjectClass(G, G.GetComponent<Renderer>().material,G.layer));
-                if (!hit.collider.tag.StartsWith("PermamentWall"))
+                if (Physics.Raycast(transform.position, /*transform.forward*/ players[x].transform.position-transform.position, out hit, 100, layerMask))
                 {
-                    G.GetComponent<Renderer>().material = tranparent;
-                    G.layer = 2;
+                    //Debug.DrawRay(transform.position, transform.TransformDirection(transform.forward/*players[x].transform.position-transform.position*/) * hit.distance, Color.yellow);
+                    if (!hit.collider.tag.StartsWith("Player"))
+                    {
+                        GameObject G = hit.collider.gameObject;
+                        transparentObjects.Push(new transparentObjectClass(G, G.GetComponent<Renderer>().material, G.layer));
+                        if (!hit.collider.tag.StartsWith("PermamentWall"))
+                        {
+                            G.GetComponent<Renderer>().material = tranparent;
+                            G.layer = 2;
+                        }
+                        else
+                        {
+                            G.layer = 17;
+                        }
+                    }
+                    else keepGoing = false;
                 }
-                else
-                {
-                    G.layer = 17;
-                }
+                else keepGoing = false;
             }
         }
-        resetCounter= (resetCounter+1)%2000;
+        resetCounter= (resetCounter+1)%20;
 
     }
     private class transparentObjectClass
