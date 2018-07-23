00a873da064782f3156d4b8670e33e5f2d98215b
Kevin12525 <40506166+Kevin12525@users.noreply.github.com>
2018-07-23 04:29:54 -0700

    Added fade Needs to fix pulsing

--- Assets/Resources/Scripts/TransparentRay.cs
+++ Assets/Resources/Scripts/TransparentRay.cs
@@ -39,33 +39,6 @@ public class TransparentRay : MonoBehaviour {
                 {
                     transparentObjectClass reset = transparentObjects.Pop();
                     reset.transparentObject.layer = reset.originalLayer;
-                    //does not work
-                    for (int x = 0; x < players.Length; x++)
-                    {
-                        bool keepGoing = true;
-                        while (keepGoing)
-                        {
-                            if (Physics.Raycast(transform.position, /*transform.forward*/ players[x].transform.position - transform.position, out hit, 100, layerMask))
-                            {
-                                //Debug.DrawRay(transform.position, transform.TransformDirection(transform.forward/*players[x].transform.position-transform.position*/) * hit.distance, Color.yellow);
-                                if (!hit.collider.tag.StartsWith("Player"))
-                                {
-                                    GameObject G = hit.collider.gameObject;
-                                    transparentObjects.Push(new transparentObjectClass(G, G.GetComponent<Renderer>().material, G.layer));
-                                    if (!hit.collider.tag.StartsWith("PermamentWall"))
-                                    {
-                                        G.layer = 2;
-                                    }
-                                    else
-                                    {
-                                        G.layer = 17;
-                                    }
-                                }
-                                else keepGoing = false;
-                            }
-                            else keepGoing = false;
-                        }
-                    }
                     reset.transparentObject.GetComponent<Renderer>().material = reset.originalMaterial;
                     reset.transparentObject.layer = reset.originalLayer;
                 }
@@ -119,7 +92,7 @@ public class TransparentRay : MonoBehaviour {
                         if (!hit.collider.tag.StartsWith("PermamentWall"))
                         {
                             //G.GetComponent<Renderer>().material = tranparent;
-                            Fade(G.GetComponent<Renderer>());
+                            StartCoroutine("Fade", G.GetComponent<Renderer>());
                             G.layer = 2;
                         }
                         else
@@ -136,11 +109,14 @@ public class TransparentRay : MonoBehaviour {
 
     }
 
-    IEnumerator Fade(Renderer r)
+    public IEnumerator Fade(Renderer r)
     {
+        Debug.Log("start");
+        //Renderer R = r;
         for (int f = 0; f < tranparent.Length; f++)
         {
             r.material = tranparent[f];
+            Debug.Log("done");
             yield return new WaitForSeconds(.1f);
         }
     }
