289522990c4e08324ed870d6f090dc044ff7bd1d
Zachary Schmalz <zacharyschmalz@gmail.com>
2018-10-12 18:45:38 -0700

    InputManager Updates
    - Added PlayerAxis & PlayerButton enums to define player actions
    - InputManager functions now require types of PlayerAxis/Button values
    - Removed PlayerInput class (essentially moved to InputManager)
    - InputManager key values in dictionaries are now the string value of PlayerAxis/Button enum
    - Updated scripts using InputManager
    - Updated prefabs

--- Assets/Scripts/Input/InputRemap.cs
+++ Assets/Scripts/Input/InputRemap.cs
@@ -25,7 +25,7 @@ public class InputRemap : MonoBehaviour
     public Color neutralColor;
     public Color selectedColor;
 
-    // Private class variables
+    //Private class variables
     private List<GameObject> controls;
     private int currentSelected;
     private int currentPlayer;
@@ -44,7 +44,7 @@ public class InputRemap : MonoBehaviour
     /// </summary>
     private IInputPlayer CurrentPlayer { get { return InputManager.Players[currentPlayer].GetComponent<IInputPlayer>(); } }
 
-    void Start ()
+    void Start()
     {
         controls = new List<GameObject>() { playerText.gameObject, inputText.gameObject };
         currentPlayer = 0;
@@ -54,75 +54,75 @@ public class InputRemap : MonoBehaviour
         CurrentSelected = 0;
         Select();
     }
-	
-	void Update ()
-    {
-        // Update the text in the event of an input change
-        if (previousMethod != CurrentPlayer.InputMethod)
-        {
-            UpdateText();
-            previousMethod = CurrentPlayer.InputMethod;
-        }
 
-        UpdateInput();
+    //    void Update()
+    //    {
+    //        // Update the text in the event of an input change
+    //        if (previousMethod != CurrentPlayer.InputMethod)
+    //        {
+    //            UpdateText();
+    //            previousMethod = CurrentPlayer.InputMethod;
+    //        }
 
-        // Do not update anything while controls are being remapped
-        if (!isRemapping)
-        {
-            // Current player text
-            if (CurrentSelected == 0)
-            {
-                if (leftHorizontal != 0 || leftVertical != 0 || right || down)
-                {
-                    Deselect();
-                    if (leftHorizontal == 1 || right)
-                        CurrentSelected++;
-                    else if (leftVertical == -1 || down)
-                        CurrentSelected += 2;
-                    Select();
-                }
-            }
+    //        UpdateInput();
 
-            // Input method text
-            else if (CurrentSelected == 1)
-            {
-                if (leftHorizontal != 0 || leftVertical != 0 || left || down)
-                {
-                    Deselect();
-                    if (leftHorizontal == -1 || left)
-                        CurrentSelected--;
-                    else if (leftVertical == -1 || down)
-                        CurrentSelected++;
-                    Select();
-                }
-            }
+    //        // Do not update anything while controls are being remapped
+    //        if (!isRemapping)
+    //        {
+    //            // Current player text
+    //            if (CurrentSelected == 0)
+    //            {
+    //                if (leftHorizontal != 0 || leftVertical != 0 || right || down)
+    //                {
+    //                    Deselect();
+    //                    if (leftHorizontal == 1 || right)
+    //                        CurrentSelected++;
+    //                    else if (leftVertical == -1 || down)
+    //                        CurrentSelected += 2;
+    //                    Select();
+    //                }
+    //            }
 
-            // Control text
-            else
-            {
-                if (leftVertical != 0 || down || up)
-                {
-                    Deselect();
-                    if (leftVertical == -1 || down)
-                        CurrentSelected++;
-                    else if (leftVertical == 1 || up)
-                        CurrentSelected--;
-                    Select();
-                }
-            }
+    //            // Input method text
+    //            else if (CurrentSelected == 1)
+    //            {
+    //                if (leftHorizontal != 0 || leftVertical != 0 || left || down)
+    //                {
+    //                    Deselect();
+    //                    if (leftHorizontal == -1 || left)
+    //                        CurrentSelected--;
+    //                    else if (leftVertical == -1 || down)
+    //                        CurrentSelected++;
+    //                    Select();
+    //                }
+    //            }
 
-            // When the player selects a player, input method, or control to change, start the respective coroutine
-            if (InputManager.GetButtonDown("A", CurrentPlayer))
-            {
-                if (CurrentSelected == 0)
-                    StartCoroutine(SwitchPlayer());
-                else if (CurrentSelected == 1)
-                    StartCoroutine(SwitchInput());
-                else
-                    StartCoroutine(RemapControl());
-            }
-        }
-	}
+    //            // Control text
+    //            else
+    //            {
+    //                if (leftVertical != 0 || down || up)
+    //                {
+    //                    Deselect();
+    //                    if (leftVertical == -1 || down)
+    //                        CurrentSelected++;
+    //                    else if (leftVertical == 1 || up)
+    //                        CurrentSelected--;
+    //                    Select();
+    //                }
+    //            }
+
+    //            // When the player selects a player, input method, or control to change, start the respective coroutine
+    //            if (InputManager.GetButtonDown("A", CurrentPlayer))
+    //            {
+    //                if (CurrentSelected == 0)
+    //                    StartCoroutine(SwitchPlayer());
+    //                else if (CurrentSelected == 1)
+    //                    StartCoroutine(SwitchInput());
+    //                else
+    //                    StartCoroutine(RemapControl());
+    //            }
+    //        }
+    //    }
 
     // Change the text color of the currently selected control
     private void Select()
@@ -138,154 +138,154 @@ public class InputRemap : MonoBehaviour
             t.color = neutralColor;
     }
 
-    // Coroutine for remapping controls
-    IEnumerator RemapControl()
-    {
-        yield return new WaitUntil(() => InputManager.GetButtonUp("A", CurrentPlayer) == true);
+    //    // Coroutine for remapping controls
+    //    IEnumerator RemapControl()
+    //    {
+    //        yield return new WaitUntil(() => InputManager.GetButtonUp("A", CurrentPlayer) == true);
 
-        Text[] text = controls[CurrentSelected].GetComponentsInChildren<Text>();
+    //        Text[] text = controls[CurrentSelected].GetComponentsInChildren<Text>();
 
-        isRemapping = true;
-        Color c = controls[CurrentSelected].GetComponentInChildren<Image>().color;
-        XboxController.XboxButton button = XboxController.XboxButton.None;
-        KeyCode key = KeyCode.None;
-        text[1].text = "";
+    //        isRemapping = true;
+    //        Color c = controls[CurrentSelected].GetComponentInChildren<Image>().color;
+    //        XboxController.XboxButton button = XboxController.XboxButton.None;
+    //        KeyCode key = KeyCode.None;
+    //        text[1].text = "";
 
-        IInputPlayer player = CurrentPlayer;
-        if (player.InputMethod == InputManager.InputMethod.Keyboard)
-        {
-            while ((key = InputManager.GetNextKeyboardButton()) == KeyCode.None)
-            {
-                c.a = Mathf.Abs(Mathf.Sin(Time.time * 3));
-                controls[CurrentSelected].GetComponentInChildren<Image>().color = c;
-                yield return new WaitForEndOfFrame();
-            }
-            if (key != KeyCode.None)
-            {
-                InputManager.RemapKeyboardButton(text[0].text, key, player);
-                text[1].text = key.ToString();
-            }
-        }
+    //        IInputPlayer player = CurrentPlayer;
+    //        if (player.InputMethod == InputManager.InputMethod.Keyboard)
+    //        {
+    //            while ((key = InputManager.GetNextKeyboardButton()) == KeyCode.None)
+    //            {
+    //                c.a = Mathf.Abs(Mathf.Sin(Time.time * 3));
+    //                controls[CurrentSelected].GetComponentInChildren<Image>().color = c;
+    //                yield return new WaitForEndOfFrame();
+    //            }
+    //            if (key != KeyCode.None)
+    //            {
+    //                InputManager.RemapKeyboardButton(text[0].text, key, player);
+    //                text[1].text = key.ToString();
+    //            }
+    //        }
 
-        else if (player.InputMethod == InputManager.InputMethod.XboxController)
-        {
-            while ((button = InputManager.GetNextXboxButton(player)) == XboxController.XboxButton.None)
-            {
-                c.a = Mathf.Abs(Mathf.Sin(Time.time * 3));
-                controls[CurrentSelected].GetComponentInChildren<Image>().color = c;
-                yield return new WaitForEndOfFrame();
-            }
-            if (button != XboxController.XboxButton.None)
-            {
-                InputManager.RemapXboxButton(text[0].text, button, player);
-                text[1].text = button.ToString();
-            }
-        }
+    //        else if (player.InputMethod == InputManager.InputMethod.XboxController)
+    //        {
+    //            while ((button = InputManager.GetNextXboxButton(player)) == XboxController.XboxButton.None)
+    //            {
+    //                c.a = Mathf.Abs(Mathf.Sin(Time.time * 3));
+    //                controls[CurrentSelected].GetComponentInChildren<Image>().color = c;
+    //                yield return new WaitForEndOfFrame();
+    //            }
+    //            if (button != XboxController.XboxButton.None)
+    //            {
+    //                InputManager.RemapXboxButton(text[0].text, button, player);
+    //                text[1].text = button.ToString();
+    //            }
+    //        }
 
-        yield return new WaitUntil(() => InputManager.GetButtonUp(text[0].text, player));
+    //        yield return new WaitUntil(() => InputManager.GetButtonUp(text[0].text, player));
 
-        c.a = 0;
-        controls[CurrentSelected].GetComponentInChildren<Image>().color = c;
-        isRemapping = false;
-    }
+    //        c.a = 0;
+    //        controls[CurrentSelected].GetComponentInChildren<Image>().color = c;
+    //        isRemapping = false;
+    //    }
 
-    // Coroutine for selecting the current players input mapping
-    IEnumerator SwitchPlayer()
-    {
-        yield return new WaitUntil(() => InputManager.GetButtonUp("A", CurrentPlayer) == true);
+    //    // Coroutine for selecting the current players input mapping
+    //    IEnumerator SwitchPlayer()
+    //    {
+    //        yield return new WaitUntil(() => InputManager.GetButtonUp("A", CurrentPlayer) == true);
 
-        isRemapping = true;
-        Color color = playerText.color;
-        color.a = 0;
+    //        isRemapping = true;
+    //        Color color = playerText.color;
+    //        color.a = 0;
 
-        IInputPlayer player = CurrentPlayer;
-        int previousPlayer = currentPlayer;
+    //        IInputPlayer player = CurrentPlayer;
+    //        int previousPlayer = currentPlayer;
 
-        while(InputManager.GetButton("A", player) == false)
-        {
-            color.a = Mathf.Abs(Mathf.Sin(Time.time * 3));
-            playerText.color = color;
+    //        while (InputManager.GetButton("A", player) == false)
+    //        {
+    //            color.a = Mathf.Abs(Mathf.Sin(Time.time * 3));
+    //            playerText.color = color;
 
-            if (leftHorizontal == -1 || left)
-            {
-                if (currentPlayer == 0)
-                    currentPlayer = InputManager.Players.Count - 1;
-                else
-                    currentPlayer--;
-                playerText.text = "Player " + (currentPlayer + 1);
-            }
+    //            if (leftHorizontal == -1 || left)
+    //            {
+    //                if (currentPlayer == 0)
+    //                    currentPlayer = InputManager.Players.Count - 1;
+    //                else
+    //                    currentPlayer--;
+    //                playerText.text = "Player " + (currentPlayer + 1);
+    //            }
 
-            else if (leftHorizontal == 1 || right)
-            {
-                if (currentPlayer == InputManager.Players.Count - 1)
-                    currentPlayer = 0;
-                else
-                    currentPlayer++;
-                playerText.text = "Player " + (currentPlayer + 1);
-            }
+    //            else if (leftHorizontal == 1 || right)
+    //            {
+    //                if (currentPlayer == InputManager.Players.Count - 1)
+    //                    currentPlayer = 0;
+    //                else
+    //                    currentPlayer++;
+    //                playerText.text = "Player " + (currentPlayer + 1);
+    //            }
 
-            yield return new WaitForEndOfFrame();
-        }
+    //            yield return new WaitForEndOfFrame();
+    //        }
 
-        yield return new WaitUntil(() => InputManager.GetButtonUp("A", CurrentPlayer) == true);
+    //        yield return new WaitUntil(() => InputManager.GetButtonUp("A", CurrentPlayer) == true);
 
-        color.a = 1;
-        playerText.color = color;
-        isRemapping = false;
+    //        color.a = 1;
+    //        playerText.color = color;
+    //        isRemapping = false;
 
-        if (previousPlayer != currentPlayer)
-            UpdateText();
-    }
+    //        if (previousPlayer != currentPlayer)
+    //            UpdateText();
+    //    }
 
-    // Coroutine for switching the input method
-    IEnumerator SwitchInput()
-    {
-        yield return new WaitUntil(() => InputManager.GetButtonUp("A", CurrentPlayer) == true);
+    //    // Coroutine for switching the input method
+    //    IEnumerator SwitchInput()
+    //    {
+    //        yield return new WaitUntil(() => InputManager.GetButtonUp("A", CurrentPlayer) == true);
 
-        isRemapping = true;
-        Color color = inputText.color;
-        color.a = 0;
+    //        isRemapping = true;
+    //        Color color = inputText.color;
+    //        color.a = 0;
 
-        IInputPlayer player = CurrentPlayer;
-        int previousMethod = (int)player.InputMethod;
-        int newMethod = previousMethod;
+    //        IInputPlayer player = CurrentPlayer;
+    //        int previousMethod = (int)player.InputMethod;
+    //        int newMethod = previousMethod;
 
-        while (InputManager.GetButton("A", player) == false)
-        {
-            color.a = Mathf.Abs(Mathf.Sin(Time.time * 3));
-            inputText.color = color;
+    //        while (InputManager.GetButton("A", player) == false)
+    //        {
+    //            color.a = Mathf.Abs(Mathf.Sin(Time.time * 3));
+    //            inputText.color = color;
 
-            if (previousMethod == (int)InputManager.InputMethod.Keyboard && InputManager.ControllersConnected == 0) { }
-            else
-            {
-                if (leftHorizontal == -1 || left)
-                    newMethod--;
-                else if (leftHorizontal == 1 || right)
-                    newMethod++;
-            }
+    //            if (previousMethod == (int)InputManager.InputMethod.Keyboard && InputManager.ControllersConnected == 0) { }
+    //            else
+    //            {
+    //                if (leftHorizontal == -1 || left)
+    //                    newMethod--;
+    //                else if (leftHorizontal == 1 || right)
+    //                    newMethod++;
+    //            }
 
-            newMethod = Mathf.Clamp(newMethod, 0, (int)Enum.GetValues(typeof(InputManager.InputMethod)).Cast<InputManager.InputMethod>().Max());
-            inputText.text = "Input Type: " + ((InputManager.InputMethod)newMethod).ToString();
-            yield return new WaitForEndOfFrame();
-        }
+    //            newMethod = Mathf.Clamp(newMethod, 0, (int)Enum.GetValues(typeof(InputManager.InputMethod)).Cast<InputManager.InputMethod>().Max());
+    //            inputText.text = "Input Type: " + ((InputManager.InputMethod)newMethod).ToString();
+    //            yield return new WaitForEndOfFrame();
+    //        }
 
-        yield return new WaitUntil(() => InputManager.GetButtonUp("A", player) == true);
+    //        yield return new WaitUntil(() => InputManager.GetButtonUp("A", player) == true);
 
-        color.a = 1;
-        inputText.color = color;
-        isRemapping = false;
+    //        color.a = 1;
+    //        inputText.color = color;
+    //        isRemapping = false;
 
-        if (previousMethod != newMethod)
-        {
-            player.InputMethod = (InputManager.InputMethod)newMethod;
-            UpdateText();
-        }
-    }
+    //        if (previousMethod != newMethod)
+    //        {
+    //            player.InputMethod = (InputManager.InputMethod)newMethod;
+    //            UpdateText();
+    //        }
+    //    }
 
     // Updates canvas text
     private void UpdateText()
     {
-        foreach(GameObject o in controls)
+        foreach (GameObject o in controls)
             if (o != playerText.gameObject && o != inputText.gameObject)
                 Destroy(o);
 
@@ -311,7 +311,7 @@ public class InputRemap : MonoBehaviour
         int j = 0;
         for (int i = 0; i < keyboardDictionary.Count; i++)
         {
-            foreach(KeyValuePair<string, KeyCode> kp in keyboardDictionary[i])
+            foreach (KeyValuePair<string, KeyCode> kp in keyboardDictionary[i])
             {
                 controls.Add(Instantiate(controlPrefab, i == 0 ? axisPanel.transform : buttonPanel.transform));
                 Text[] text = controls[j + 2].GetComponentsInChildren<Text>();
@@ -337,7 +337,7 @@ public class InputRemap : MonoBehaviour
         Dictionary<string, XboxController.XboxButton> xboxButtonDictionary = InputManager.GetXboxButtonDictionary(currentPlayer + 1);
 
         int i = 0;
-        foreach(KeyValuePair<string, XboxController.XboxAxis> kp in xboxAxisDictionary)
+        foreach (KeyValuePair<string, XboxController.XboxAxis> kp in xboxAxisDictionary)
         {
             controls.Add(Instantiate(controlPrefab, axisPanel.transform));
             Text[] text = controls[i + 2].GetComponentsInChildren<Text>();
@@ -370,16 +370,16 @@ public class InputRemap : MonoBehaviour
         }
     }
 
-    // Update input values from the current player
-    private void UpdateInput()
-    {
-        IInputPlayer player = CurrentPlayer;
+    //    // Update input values from the current player
+    //    private void UpdateInput()
+    //    {
+    //        IInputPlayer player = CurrentPlayer;
 
-        leftHorizontal = InputManager.GetAxisDown("LeftHorizontal", player);
-        leftVertical = InputManager.GetAxisDown("LeftVertical", player);
-        up = InputManager.GetButtonDown("Up", player);
-        right = InputManager.GetButtonDown("Right", player);
-        down = InputManager.GetButtonDown("Down", player);
-        left = InputManager.GetButtonDown("Left", player);
-    }
+    //        leftHorizontal = InputManager.GetAxisDown("LeftHorizontal", player);
+    //        leftVertical = InputManager.GetAxisDown("LeftVertical", player);
+    //        up = InputManager.GetButtonDown("Up", player);
+    //        right = InputManager.GetButtonDown("Right", player);
+    //        down = InputManager.GetButtonDown("Down", player);
+    //        left = InputManager.GetButtonDown("Left", player);
+    //    }
 }
