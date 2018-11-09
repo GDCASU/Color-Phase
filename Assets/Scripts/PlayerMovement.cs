075588f10fce7cd3513af38b49bfc09fca673755
content-deleted <contentdeleted@gmail.com>
2018-11-09 14:04:27 -0700

    wip: more fields for player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -23,6 +23,21 @@ public class PlayerMovement : MonoBehaviour
     private static readonly float axisModifier = Mathf.Sqrt(2) / 2;
     private static readonly float pushModifier = 50f;
 
+    #region Jump Parm
+    Vector3 forceOld = Vector3.zero;
+    bool jumpingDown = false;
+    public float hangTime = 1f;
+    public float fallSpeedCap = 10;
+    public float fallCoefficent = 1;
+
+    #endregion
+
+    #region Move Param
+    public float moveSpeedCap = 10;
+
+    #endregion
+
+    Vector3 force;
     private void Start()
     {
         player = GetComponent<IInputPlayer>();
@@ -43,6 +58,7 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Speed", Mathf.Sqrt(Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.z, 2)));
         animator.SetBool("Grounded", grounded);
     }
+
     private void Move()
     {
         // Movement Input
@@ -65,24 +81,36 @@ public class PlayerMovement : MonoBehaviour
         if (Physics.Raycast(transform.position, Vector3.down, playerCollider.bounds.extents.y))
         {
             // TODO: Detect if it is a valid platform (Not a moving object)
+            if(!grounded) jumpingDown = false;
             grounded = true;
             ledgeMemory = transform.position; // Remember the ledge position of the player
             if (InputManager.GetButtonDown(PlayerButton.Jump, player))
             {
                 animator.SetTrigger("Jump");
                 rb.velocity = new Vector3(rb.velocity.x, jumpStrength, rb.velocity.z);
+                jumpingDown = true;
             }
         }
-        else if(!Physics.Raycast(playerCollider.bounds.center, Vector3.down, playerCollider.bounds.extents.y + 1)) grounded = false; 
 
-        //uncomment to prevent movement mid-air
-        //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), 0.81f))
-        {
-            Vector3 force = cam.transform.forward.normalized * zAxis * speed + cam.transform.right.normalized * xAxis * speed/2;
-            force.y = 0;
-            rb.AddForce(force, ForceMode.Acceleration);
+        else if(!Physics.Raycast(playerCollider.bounds.center, Vector3.down, playerCollider.bounds.extents.y + 1)){
+            if (!InputManager.GetButton(PlayerButton.Jump, player) || rb.velocity.y < -hangTime) 
+                jumpingDown = false;
+            grounded = false; 
+            if(!jumpingDown) rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y - fallCoefficent, rb.velocity.z);
         }
+        //uncomment to prevent movement mid-air
+        //if (grounded)
+        {
+            force = cam.transform.forward.normalized * zAxis * speed + cam.transform.right.normalized * xAxis * speed/2;
+            force.y = 0;
+        }
+
+        rb.AddForce( (grounded) ? force : (force + forceOld) / 2 , ForceMode.Impulse);
         rotatePlayer(xAxis, zAxis);
+
+        rb.velocity = clampVelocities(rb.velocity);
+
+        if(grounded) forceOld = force;
     }
     
     /// <summary>
@@ -103,11 +131,28 @@ public class PlayerMovement : MonoBehaviour
             if (Mathf.Abs(playerRotation - cameraRotation) > angleToSnap)
                 transform.rotation = Quaternion.Lerp(this.transform.rotation, cam.transform.rotation, lookSpeed * Time.deltaTime);
             else transform.rotation = cam.transform.rotation;
-
-            // Makes sure that the x and z rotations are 0
-            transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
-
         }
         else rb.freezeRotation = true;
+        
+        // Makes sure that the x and z rotations are 0
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+
+    }
+
+    private Vector3 clampVelocities(Vector3 velocity) {
+        Vector3 vOut = velocity;
+
+        Vector3 moveSpeed = new Vector3(velocity.x, 0, velocity.z);
+
+        if(moveSpeed.magnitude > moveSpeedCap) 
+            vOut = moveSpeed.normalized * moveSpeedCap;
+
+        float ySpeed = velocity.y;
+        if(!grounded && ySpeed < -fallSpeedCap) 
+            ySpeed = -fallSpeedCap;
+
+        vOut = new Vector3(vOut.x, ySpeed, vOut.z);
+
+        return vOut;
     }
 }
