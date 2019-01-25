7f3df1db189f530dae0a435f5c2dfc13e0596efd
danielwaker <dver99@sbcglobal.net>
2019-01-25 16:00:31 -0700

    Conveyor Belt
    Conveyor belt moves the player along the belt.

---
+++ Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    //speed of belt
+    public float beltVelocity = 100;
+
+    //direction the player goes in
+    public bool reverseDirection = false;
+
+    void OnCollisionStay(Collision obj)
+    {
+        if (reverseDirection == false)
+        {
+            obj.gameObject.GetComponent<Rigidbody>().velocity = obj.gameObject.GetComponent<Rigidbody>().velocity + beltVelocity * Time.deltaTime * transform.right;
+        }
+
+        else
+        {
+            obj.gameObject.GetComponent<Rigidbody>().velocity = obj.gameObject.GetComponent<Rigidbody>().velocity + beltVelocity * Time.deltaTime * -transform.right;
+        }
+    }
+}
