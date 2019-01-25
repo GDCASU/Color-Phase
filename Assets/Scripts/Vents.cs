1b5880e4fc8505798ef886fa25ff575dc5231fe4
propredator23 <christiandash@hotmail.com>
2019-01-25 16:20:09 -0700

    Vents
    created a vent, see further details on how to use them design

---
+++ Assets/Scripts/Vents.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vents : MonoBehaviour
+{
+    public float vent_strength = 0;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == ("Player"))
+        {
+            other.GetComponent<Rigidbody>().velocity = other.GetComponent<Rigidbody>().velocity + transform.up * vent_strength;
+        }
+    }
+    
+}
