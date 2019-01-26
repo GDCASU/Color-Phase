c6ccf48d3e89168204f50f303438aeadc215aefa
Kevin12525 <40506166+Kevin12525@users.noreply.github.com>
2019-01-26 11:10:09 -0700

    added some securitycam code

---
+++ Assets/SecurityCamBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCamBehavior : MonoBehaviour {
+    bool looking;
+    bool found=false;
+
+
+	// Use this for initialization
+	void Start () {
+        looking = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        if (looking)
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
+            {
+                if (hit.collider.tag.StartsWith("Player"))
+                {
+                    found = true;
+                }
+            }
+        }
+        if (found)
+        {
+            transform.LookAt(hit.transform);
+        }
+	}
+
+}
