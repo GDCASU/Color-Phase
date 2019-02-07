8e76dc7da4dded42f5b260c2b969eaf3b96a54ed
mawebbe1 <mawebbe1@asu.edu>
2019-02-06 18:46:58 -0700

    Cracked floor stuff

--- Assets/Scripts/Interactable/crackedFloor.cs
+++ Assets/Scripts/Interactable/crackedFloor.cs
@@ -6,23 +6,10 @@ public class crackedFloor : MonoBehaviour {
 
     public GameObject objReplace;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     private void OnTriggerEnter(Collider other)
     {
-
-
         GameObject wreck = (GameObject)Instantiate(objReplace, transform.position, transform.rotation);
         Destroy(gameObject);
-
     }
 
 }
