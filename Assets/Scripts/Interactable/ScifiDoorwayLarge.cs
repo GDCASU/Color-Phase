ae7598fb56f6fd7690ecbf00c66b9ee96614b24f
mawebbe1 <mawebbe1@asu.edu>
2019-03-15 01:14:53 -0700

    Added large doors, updated levels, and fixed door script

--- Assets/Scripts/Interactable/ScifiDoorwayLarge.cs
+++ Assets/Scripts/Interactable/ScifiDoorwayLarge.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class ScifiDoorwayLarge : MonoBehaviour {
 
-
     public GameObject doorLeft;
     public GameObject doorRight;
     public GameColor colorValue = GameColor.Red;
-    public static float timer;
+    public float move;
 
-    public bool open;
+    private bool open;
 
     //Stop must be initialized as false on the first teleporter and true on the second.
     //public bool stop; // WHY? - stop is not used in the script.
@@ -22,37 +21,36 @@ public class ScifiDoorwayLarge : MonoBehaviour {
     void Start()
     {
         doorPos = doorLeft.transform.position;
-        timer = 0f;
+        move = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (open == false && timer > 0f)
+        if (open == false && move > 0f)
         {
-            timer = timer - 0.05f; // close door
-        } else
+            move = move - 0.05f; // close door
+        }
+        if (open == true && move < 1.8f)
         {
-            if (open == true && timer < 1.8f)
-            {
-                timer = timer + 0.05f; // open door
-            }
+            move = move + 0.05f; // open door
         }
 
-        if (timer < 0f)
+        if (open == false && move < 0f)
         {
-            timer = 0f;
+            move = 0f;
         }
-        if (timer > 1.8f)
+        if (open == true && move > 1.8f)
         {
-            timer = 1.8f;
+            move = 1.8f;
         }
 
-        doorLeft.transform.localPosition = doorPos + new Vector3(timer, 0, 0) - transform.localPosition;
-        doorRight.transform.localPosition = doorPos + new Vector3(-timer, 0, 0) - transform.localPosition;
-
+        doorLeft.transform.localPosition = doorPos + new Vector3(move, 0, 0) - transform.localPosition;
+        doorRight.transform.localPosition = doorPos + new Vector3(-move, 0, 0) - transform.localPosition;
+        
     }
 
+    
     public void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -65,12 +63,18 @@ public class ScifiDoorwayLarge : MonoBehaviour {
         }
     }
 
+    
     public void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            open = false;
+            if (other.GetComponent<ColorState>().currentColor == colorValue || neutralColor == true)
+            {
+                open = false;
+            }
         }
     }
+    
+    
 
 }
