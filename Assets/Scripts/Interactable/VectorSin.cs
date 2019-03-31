f3d2b87ef8ed63e1de63bb48128503c631ec70b4
content-deleted <contentdeleted@gmail.com>
2019-03-31 11:05:01 -0700

    feat: improved player location reset, platforms

---
+++ Assets/Scripts/Interactable/VectorSin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorSin : MonoBehaviour {
+
+	public Vector3 axis;
+    public float speed;
+    private Rigidbody rb;
+    private Vector3 st;
+    void Start () { 
+        rb = GetComponent<Rigidbody>(); 
+        st = transform.position;
+    }
+    private float t;
+	void Update () {
+        t+=Time.deltaTime;
+		//rb.MovePosition(Mathf.Sin(t*speed)*axis+st);
+        transform.position = Mathf.Sin(t*speed)*axis+st;
+	}
+}
