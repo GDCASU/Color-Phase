c88871ce818a2eb9d095214fa3c4641f04a100c8
propredator23 <christiandash@hotmail.com>
2019-04-05 12:42:21 -0700

    Good Swing
    Best swing that ive been able to do yet

--- Assets/Scripts/Player/Grapple.cs
+++ Assets/Scripts/Player/Grapple.cs
@@ -34,6 +34,8 @@ public class Grapple : MonoBehaviour
     private bool isGrappled;
     private bool canGrapple;
     private bool swinging;
+    private bool reset;
+    private bool grounded;
 
     // Object to use in calcualtions
     private Collider col;
@@ -43,10 +45,16 @@ public class Grapple : MonoBehaviour
     private Transform grappleAnchor;
     private LineRenderer line;
     private RaycastHit hit;
-    
+    private Vector3 v;
+    private Vector3 minSwing;
+
+
     private GameObject target;
+    private float swingXDirection;
+
     void Awake()
     {
+        reset = true;
         rb = GetComponent<Rigidbody>();
         col = GetComponent<Collider>();
         line = gameObject.AddComponent<LineRenderer>();
@@ -55,9 +63,59 @@ public class Grapple : MonoBehaviour
         grappleAnchor = new GameObject().transform;
         state = GetComponent<ColorState>();
     }
-
-    void Update()
+    private bool OnScreen(Vector3 worldPos)
     {
+        var vP = Camera.main.WorldToViewportPoint(worldPos);
+        return vP.x > 0 && vP.x < 1 && vP.y > 0 && vP.y < 1;
+    }
+    public void LateUpdate()
+    {
+        if (!isGrappled && !canGrapple)
+        {
+            var t = GrappleTarget.targets.Where(x => (x.neutral == true || (x.PushPull && (state.currentColor == GameColor.Red || state.currentColor == GameColor.Green)) || x.targetColor == state.currentColor)
+                                                && Vector3.Distance(x.transform.position, transform.position) <= hookRange
+                                                && Vector3.Dot(x.transform.position - Camera.main.transform.position, Camera.main.transform.forward) >= 0
+                                                && OnScreen(x.transform.position))
+                .OrderBy(p => Vector2.Distance(Camera.main.WorldToViewportPoint(p.transform.position), new Vector2(0.5f, 0.5f)))
+                .FirstOrDefault();
+
+            // ADD THIS BACK TO ORDER QUERY LATER FOR SMOOTHING OVER DISTANCE
+            //Vector3.Distance(p.transform.position,transform.position)+100*V
+
+            RaycastHit r;
+            if (t != null && Physics.Raycast(transform.position, t.transform.position - transform.position, out r, hookRange) && r.transform == t.transform)
+            {
+                hit = r;
+                target = t.gameObject;
+            }
+            else
+            {
+                target = null;
+                hit = new RaycastHit();
+            }
+        }
+
+        RaycastHit rh;
+        if (target != null && Physics.Raycast(Camera.main.transform.position, target.transform.position - Camera.main.transform.position, out rh, hookRange) && rh.transform == target.transform)
+        {
+            reticle.SetActive(true);
+            reticle.transform.position = Camera.main.WorldToScreenPoint(target.transform.position);
+        }
+        else
+            reticle.SetActive(false);
+
+    }
+
+    void FixedUpdate()
+    {
+        if (grounded && !Physics.Raycast(GetComponent<Collider>().bounds.center, Vector3.down, GetComponent<Collider>().bounds.extents.y + 0.5f))
+        {
+            grounded = false;
+        }
+        else
+        {
+            grounded = true;
+        }
         if (InputManager.GetButtonDown(PlayerButton.Grapple) && !Box.Holding)
         {
             if (target != null)
@@ -70,19 +128,19 @@ public class Grapple : MonoBehaviour
         if (isGrappled)
         {
             GameColor color = col.GetComponent<ColorState>().currentColor;
-            swinging = color == GameColor.Yellow;
+            swinging = color == GameColor.Red;
             switch (color)
             {
-                case GameColor.Red:
+                case GameColor.Yellow:
                     GrapplePullObject();
                     break;
-                case GameColor.Green: 
+                case GameColor.Green:
                     GrapplePushObject();
                     break;
                 case GameColor.Blue:
                     GrapplePullPlayer();
                     break;
-                case GameColor.Yellow:
+                case GameColor.Red:
                     GrappleSwing();
                     break;
             }
@@ -103,62 +161,64 @@ public class Grapple : MonoBehaviour
 
         if (InputManager.GetButtonUp(PlayerButton.Grapple))
         {
+            var s = swinging;
             disableGrapple();
-        }
-    }
-    private bool OnScreen(Vector3 worldPos){
-        var vP = Camera.main.WorldToViewportPoint(worldPos);
-        return vP.x>0 && vP.x<1 && vP.y>0 &&vP.y <1;
-    }
-    public void LateUpdate () {
-        if(!isGrappled && !canGrapple) {
-            var t = GrappleTarget.targets.Where(x=> (x.neutral==true || (x.PushPull && (state.currentColor == GameColor.Red || state.currentColor == GameColor.Green)) || x.targetColor==state.currentColor) 
-                                                && Vector3.Distance(x.transform.position,transform.position) <= hookRange 
-                                                && Vector3.Dot(x.transform.position - Camera.main.transform.position, Camera.main.transform.forward) >= 0
-                                                && OnScreen(x.transform.position))
-                .OrderBy (p => Vector2.Distance(Camera.main.WorldToViewportPoint(p.transform.position), new Vector2(0.5f,0.5f)))
-                .FirstOrDefault();
-
-            // ADD THIS BACK TO ORDER QUERY LATER FOR SMOOTHING OVER DISTANCE
-            //Vector3.Distance(p.transform.position,transform.position)+100*V
-
-            RaycastHit r;
-            if (t != null && Physics.Raycast(transform.position, t.transform.position - transform.position, out r, hookRange) && r.transform == t.transform) {
-                hit = r;
-                target = t.gameObject;
-            }
-            else
+            if (s)
             {
-                target = null;
-                hit = new RaycastHit();
+                reset = true;
+                rb.velocity *= 1.5f;
             }
         }
-
-        RaycastHit rh;
-        if (target != null && Physics.Raycast(Camera.main.transform.position, target.transform.position - Camera.main.transform.position, out rh, hookRange) && rh.transform == target.transform) {
-            reticle.SetActive(true);
-            reticle.transform.position = Camera.main.WorldToScreenPoint( target.transform.position );
-        }
-        else 
-            reticle.SetActive(false);
-
-    }
-
-    void FixedUpdate()
-    {
+        print(reset);
         // Only let player add force if they are below a certain point in relation to the hook anchor point
-        if (swinging && transform.position.y < hookAnchor.position.y - 10)
+        if (swinging && !grounded)
         {
-            // Get orthogonal vector to player
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y - 1, rb.velocity.z);
+            GetComponentInParent<PlayerMovement>().enabled = false;
+            transform.forward = gameObject.transform.parent.GetComponentInChildren<Camera>().transform.forward;
             Vector3 player = transform.forward;
             Vector3 rope = hookAnchor.position - transform.position;
             Vector3.OrthoNormalize(ref rope, ref player);
 
-            float x = InputManager.GetAxis(PlayerAxis.MoveHorizontal);
-            float z = InputManager.GetAxis(PlayerAxis.MoveVertical);
+            Vector3 swingZDirection = Vector3.Cross(v, transform.right);
+            Vector3 swingXDirection = Vector3.Cross(v, transform.forward);
 
-            rb.AddForce(transform.right * x * swingStrafeSpeed);
-            rb.AddForce(player * z * swingSpeed);
+            float z = InputManager.GetAxis(PlayerAxis.MoveVertical);
+            float x = InputManager.GetAxis(PlayerAxis.MoveHorizontal);
+
+
+            if (transform.position.y < (hookAnchor.position.y - (ropeLength * .99)))
+            {
+                reset = true;
+            }   
+            else if (rb.velocity.y < 0)
+            {
+                if (InputManager.GetAxis(PlayerAxis.MoveVertical) == 1 && Vector3.Angle(transform.forward, v) > 90f)
+                {
+                    print("onlyy on forwad");
+                    applySwingForce(swingZDirection, swingXDirection,z,x);
+                }
+                else
+                {
+                    reset = false;
+                }
+            }
+            if ((transform.position.y < (hookAnchor.position.y - (ropeLength * .15))))
+            {
+                if (reset)
+                {
+                    print("bad force");
+                    applySwingForce(swingZDirection, swingXDirection, z, x);
+                }
+            }
+            else
+            {
+                reset = false;
+            }
+        }
+        else
+        {
+            GetComponentInParent<PlayerMovement>().enabled = true;
         }
     }
 
@@ -222,7 +282,8 @@ public class Grapple : MonoBehaviour
     // Lets the user swing around from a fixed point
     private void GrappleSwing()
     {
-        Vector3 v = transform.position - hookAnchor.position;
+        minSwing = transform.position;
+        v = transform.position - hookAnchor.position;
         float distance = v.magnitude;
 
         // Draws an imaginary sphere around the player, simulating the rope length.
@@ -239,9 +300,14 @@ public class Grapple : MonoBehaviour
         }
 
         // Checks the player's distance to the ground and shortens it will player will touch the floor.
-        if (Physics.Raycast(transform.position, Vector3.down, col.bounds.extents.y + 1))
+        if (Physics.Raycast(transform.position, Vector3.down, col.bounds.extents.y + 1) &&  grounded)
         {
             ropeLength -= .15f;
         }
+    }
+    public void applySwingForce( Vector3 swingZDirection, Vector3 swingXDirection, float z, float x)
+    {
+        rb.AddForce(swingZDirection * z * swingSpeed);
+        rb.AddForce(swingXDirection * -x * swingStrafeSpeed);
     }
 }
