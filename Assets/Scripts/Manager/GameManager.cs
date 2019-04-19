4d663af316ddf03884c6b5f456d6a5c620685bfd
content-deleted <contentdeleted@gmail.com>
2019-04-19 10:45:52 -0700

    feat: saving and loading functions in game manager

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
 /* Author:      Zachary Schmalz & Jacob Hann
  * Version:     1.1.0
  * Date:        April 4, 2019
@@ -13,6 +17,7 @@ public class GameManager : MonoBehaviour
 {
     private static GameManager singleton;
     private static int latestUnlocked;
+    const string saveName = "ColorPhase.dat";
     void Awake()
     {
         if (singleton == null)
@@ -33,10 +38,52 @@ public class GameManager : MonoBehaviour
     void Start ()
     {
 		// Get the save file
+        LoadGame ();
 	}
     
     static void updateSaveData (Scene scene, LoadSceneMode sceneMode) {
         if(scene.buildIndex > latestUnlocked) latestUnlocked = scene.buildIndex;
         // write to save file
+        SaveGame ();
+    }
+
+    static bool SaveGame () {
+        bool saved = true;
+        FileStream fs = new FileStream(Application.persistentDataPath+saveName, FileMode.Create);
+        BinaryFormatter formatter = new BinaryFormatter();
+        try 
+        {
+            formatter.Serialize(fs, latestUnlocked);
+        }
+        catch (SerializationException e) 
+        {
+            Debug.Log("Saving Failed. Reason: " + e.Message);
+            saved = false;
+        }
+        finally 
+        {
+            fs.Close();
+        }
+        return saved;
+    }
+
+    static bool LoadGame () {
+        bool loaded = true;
+        FileStream fs = new FileStream(Application.persistentDataPath+saveName, FileMode.Open);
+        try 
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            latestUnlocked = (int) formatter.Deserialize(fs);
+        }
+        catch (SerializationException e) 
+        {
+            Debug.Log("Failed to load save. Reason: " + e.Message);
+            loaded = false;
+        }
+        finally 
+        {
+            fs.Close();
+        }
+        return loaded;
     }
 }
