e06a798012f3d14c80e99f608bf2ec633a2338a9
propredator23 <christiandash@hotmail.com>
2019-04-19 09:58:31 -0700

    So jacob can use
    Not fully done yet

---
+++ Assets/ButtonProperties.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonProperties : MonoBehaviour
+{
+    public Scene scene;
+    public bool available;
+	// Use this for initialization
+    
+    public void SetScene(Scene passed)
+    {
+        scene = passed;
+    }
+    public void MakeAvailable()
+    {
+        available = true;
+    }
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene(scene.name);
+    }
+
+}
--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -10,41 +10,121 @@ public class PauseMenu : MonoBehaviour
 {
     UI playerUI;
     IInputPlayer player;
+    PlayerCamControl camControl;
+    PlayerMovement playerMovement;
+    public Scene[] scenes; 
     public GameObject pauseMenu;
     public GameObject HUD;
-    public bool isPaused=false;
+    public GameObject selectLevel;
+    public GameObject ArrowPrefab;
+    private bool isPaused = false;
+    private bool building = false;
+    int index;
+    public Button buttonPrefab;
+    Button button;
+    int numerOfScenes;
+    Scene scene;
 
     private void Start()
     {
-        GetComponent<IInputPlayer>();
+        numerOfScenes= SceneManager.sceneCount;
+        index = 0;
+        player = GetComponent<IInputPlayer>();
         playerUI = GetComponent<UI>();
-
+        camControl = gameObject.transform.parent.transform.parent.GetComponentInChildren<PlayerCamControl>();
+        playerMovement = gameObject.transform.parent.transform.parent.GetComponentInChildren<PlayerMovement>();
     }
     private void Update()
     {
-       if(InputManager.GetButtonDown(PlayerInput.PlayerButton.UI_Submit, player))
+        if (InputManager.GetButtonDown(PlayerInput.PlayerButton.UI_Submit, player))
         {
-            if(playerUI.enabled && !isPaused)
+            if (!isPaused && pauseMenu.active==false)
             {
-                isPaused = true;
-                playerUI.enabled = false;
-                HUD.SetActive(false);
+                Pause();
+            }
+            else if(selectLevel.active==true)
+            {
+                selectLevel.SetActive(false);
                 pauseMenu.SetActive(true);
-
             }
             else
             {
-                isPaused = false;
-                pauseMenu.SetActive(false);
-                playerUI.enabled = true;
-                HUD.SetActive(true);
+                ResumeGame();
             }
-
         }
 
+    }
+    public void FixedUpdate()
+    {
+        if (index < 10 && index <  numerOfScenes && building==true)
+        {
+            BuildLevelsUI();
+            index++;
+        }
+        else
+        {
+            building = false;
+        }
+    }
+    public void Pause()
+    {
+        isPaused = true;
+        camControl.enabled = false;
+        playerMovement.enabled = false;
+        playerUI.enabled = false;
+        HUD.SetActive(false);
+        pauseMenu.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        camControl.enabled = true;
+        playerMovement.enabled = true;
+        playerUI.enabled = true;
+        HUD.SetActive(true);
+    }
+    public void SaveGame()
+    {
 
     }
-    public void resumeGame()
+    public void LevelSelect()
+    {
+        pauseMenu.SetActive(false);
+        selectLevel.SetActive(true);
+        building = true;
+
+        
+    }
+    public void BuildLevelsUI()
+    {
+        float xPosition = 160;
+        float yPosition = -90;
+        scene = SceneManager.GetSceneAt(index);
+        button = Instantiate(buttonPrefab, Vector2.zero, Quaternion.identity);
+        button.transform.parent = selectLevel.transform;
+        if (index < 5)
+        {
+            button.GetComponent<RectTransform>().localPosition = new Vector2(-xPosition,180- (yPosition*index));
+        }
+        else
+        {
+            button.GetComponent<RectTransform>().localPosition = new Vector2(xPosition, 180 - (yPosition * index-5));
+        }
+        button.GetComponent<RectTransform>().localScale =Vector3.one;
+        button.GetComponent<ButtonProperties>().SetScene(scene);
+        button.GetComponentInChildren<Text>().text = scene.name;
+
+    }
+    public void Settings()
+    {
+
+    }
+    public void ExitGame()
     {
 
     }
