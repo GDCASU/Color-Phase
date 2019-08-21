6a057a0b72ff253e3273481008282e6760fc5c8c
content-deleted <contentdeleted@gmail.com>
2019-08-20 22:14:32 -0400

    fix: config for prefabs and level reorder

--- Assets/Scripts/Interactable/QuickSwapPickup.cs
+++ Assets/Scripts/Interactable/QuickSwapPickup.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class QuickSwapPickup : MonoBehaviour {
 	public GameColor colorValue;
-    public bool destroyOnContact = false;
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag.StartsWith("Player")) Destroy(gameObject);
@@ -13,10 +12,14 @@ public class QuickSwapPickup : MonoBehaviour {
         // Create a new follower usually 
         if(PlayerColorController.singleton.GetComponent<QuickSwap>() == null ){
             GameObject follower = Instantiate(Resources.Load("Prefabs/Player/SwapFollower") as GameObject);
+            var followerColor = follower.GetComponent<ColorState>();
+            followerColor.currentColor = colorValue;
             follower.transform.position = this.transform.position;
             PlayerColorController.singleton.gameObject.AddComponent<QuickSwap>();
 
-            PlayerColorController.singleton.GetComponent<QuickSwap>().followerState = follower.GetComponent<ColorState>();
+            var quickSwapColor = PlayerColorController.singleton.GetComponent<QuickSwap>();
+            quickSwapColor.followerState = followerColor;
+            quickSwapColor.storedColor = followerColor.currentColor;
         }
         // If for some reason we have levels with multiple of these we'll just set the color
         else {
