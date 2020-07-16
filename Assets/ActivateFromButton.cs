e6dbb061a5c6c82b000ae50ec04d1ae4c1cb969f
content-deleted <contentdeleted@gmail.com>
2020-07-16 11:55:59 -0400

    feat: new level using desert planet assets

---
+++ Assets/ActivateFromButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateFromButton : MonoBehaviour {
+
+	public List<MonoBehaviour> ActiveBehaviours = new List<MonoBehaviour>();
+    public ButtonToggle button;
+
+    private bool internalState;
+    void Start () {
+        internalState = button.state;
+    }
+	void Update () {
+		if(button.state != internalState) {
+            ActiveBehaviours.ForEach(x => x.enabled = button.state);
+        }
+	}
+}
