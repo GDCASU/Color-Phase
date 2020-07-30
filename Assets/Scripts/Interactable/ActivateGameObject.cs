934db4a7a7a5709ea8855716ac7c2bd71f467af7
content-deleted <contentdeleted@gmail.com>
2020-07-30 16:23:12 -0400

    feat: new button control scripts for general logic

---
+++ Assets/Scripts/Interactable/ActivateGameObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateGameObject : MonoBehaviour {
+
+	public List<GameObject> ActiveObjects = new List<GameObject>();
+	public List<GameObject> InactiveObjects = new List<GameObject>();
+    public ButtonToggle button;
+
+    private bool internalState;
+    void Start () {
+        internalState = button.state;
+    }
+	void Update () {
+		if(button.state != internalState) {
+            ActiveObjects.ForEach(x => x.SetActive(button.state));
+            InactiveObjects.ForEach(x => x.SetActive(!button.state));
+        }
+	}
+}
