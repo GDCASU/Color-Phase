8747deab0b724633120a5783929a326fe7effbe2
content-deleted <contentdeleted@gmail.com>
2020-08-17 14:59:27 -0400

    feat: added a steam achievement for micheal

---
+++ Assets/Scripts/Steamworks.NET/AchievementOnEnter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+public class AchievementOnEnter : MonoBehaviour {
+    public string AchievementName;
+	private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player") && SteamManager.Initialized) {
+            bool userHasAchievement = false;
+            SteamUserStats.GetAchievement(AchievementName, out userHasAchievement);
+            if(!userHasAchievement) {
+                SteamUserStats.SetAchievement(AchievementName);
+                SteamUserStats.StoreStats();
+            }
+        }
+    }
+}
